baa3c73373af4cf08b1fad5c445dbc8f89bd1900
Sankar <1890648+srgsanky@users.noreply.github.com>
2024-08-28 22:58:07 -0700

    Use in-built display as primary display by default and show polybar only on active monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -12,7 +12,8 @@ POLYBAR_CONFIG_FILE=~/.config/i3/polybar/config.ini
 
 echo "---" | tee -a /tmp/polybar.log
 if type "xrandr"; then
-    for m in $(xrandr --query | grep " connected" | cut -d " " -f1)
+    # Show only on active monitors
+    for m in $(xrandr --listactivemonitors | grep DP- | cut -f 6 -d ' ')
     do
         MONITOR=$m polybar --reload -c $POLYBAR_CONFIG_FILE $POLYBAR_NAME | tee -a /tmp/polybar.log & disown
     done
